eb164d6e01cf07c222dcc25ccc33888e4ee05037
DrUnlimitedGG <epicplayzquestions@gmail.com>
2024-07-11 13:30:32 -0400

    Revert "working on export panel"
    This reverts commit 555f0478929c6279e4dec2d2d79e5ee7ed8492d2.

--- src/Workspace/MainModule/Evaluation/Phase3/UIGradient/LocalScript.client.lua
+++
@@ -1,3 +0,0 @@
-game:GetService("RunService").RenderStepped:Connect(function()
-	script.Parent.Rotation += 1
-end)
--- src/Workspace/MainModule/Evaluation/Phase4/OpenPanel/LocalScript.client.lua
+++
@@ -1,3 +0,0 @@
-script.Parent.MouseButton1Click:Connect(function()
-	script.Parent.Parent.Parent.QuizPanel:TweenPosition(UDim2.new(0.5, 0, 0.5, 0))
-end)
