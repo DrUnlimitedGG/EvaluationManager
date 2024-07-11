3fe9a972fa421a2f41aa17cfd28f87ec6b0aac15
DrUnlimitedGG <epicplayzquestions@gmail.com>
2024-07-11 13:37:53 -0400

    Revert "working on export panel"
    This reverts commit 462abf553e8e7ca10dbb3d93904bd5dc62c2adcc.

--- src/StarterGui/Evaluation/EvalEnd/Folder/LocalScript.client.lua
+++ src/StarterGui/Evaluation/EvalEnd/Folder/LocalScript.client.lua
@@ -14,30 +14,33 @@ end)
 
 script.Parent.Phase2.MouseButton1Click:Connect(function()
 	exportoptions.Phase2.Value = not exportoptions.Phase2.Value
-	
-	if exportoptions.Phase2.Value == false then
-		script.Parent.Phase2.Image = ""
-	elseif exportoptions.Phase2.Value then
-		script.Parent.Phase2.Image = "http://www.roblox.com/asset/?id=12690727184"
+
+	if exportoptions.Phase1.Value == false then
+		script.Parent.Phase1.Image = ""
+	elseif exportoptions.Phase1.Value then
+		script.Parent.Phase1.Image = "http://www.roblox.com/asset/?id=12690727184"
 	end
+
 end)
 
-script.Parent.Phase3.MouseButton1Click:Connect(function()
-	exportoptions.Phase3.Value = not exportoptions.Phase3.Value
-	
-	if exportoptions.Phase3.Value == false then
-		script.Parent.Phase3.Image = ""
-	elseif exportoptions.Phase3.Value then
-		script.Parent.Phase3.Image = "http://www.roblox.com/asset/?id=12690727184"
+script.Parent.Phase1.MouseButton1Click:Connect(function()
+	exportoptions.Phase1.Value = not exportoptions.Phase1.Value
+
+	if exportoptions.Phase1.Value == false then
+		script.Parent.Phase1.Image = ""
+	elseif exportoptions.Phase1.Value then
+		script.Parent.Phase1.Image = "http://www.roblox.com/asset/?id=12690727184"
 	end
+
 end)
 
-script.Parent.Phase4.MouseButton1Click:Connect(function()
-	exportoptions.Phase4.Value = not exportoptions.Phase4.Value
-	
-	if exportoptions.Phase4.Value == false then
-		script.Parent.Phase4.Image = ""
-	elseif exportoptions.Phase4.Value then
-		script.Parent.Phase4.Image = "http://www.roblox.com/asset/?id=12690727184"
+script.Parent.Phase1.MouseButton1Click:Connect(function()
+	exportoptions.Phase1.Value = not exportoptions.Phase1.Value
+
+	if exportoptions.Phase1.Value == false then
+		script.Parent.Phase1.Image = ""
+	elseif exportoptions.Phase1.Value then
+		script.Parent.Phase1.Image = "http://www.roblox.com/asset/?id=12690727184"
 	end
+
 end)
--- src/StarterGui/Evaluation/EvalEnd/TextButton/LocalScript.client.lua
+++ src/StarterGui/Evaluation/EvalEnd/TextButton/LocalScript.client.lua
@@ -1,6 +1,4 @@
-script.Parent.MouseButton1Click:Connect(function()
-	wait(2)
-	
+script.Parent.MouseButton1Click:Connect(function()	
 	script.Parent.Parent.Visible = false
 	script.Parent.Parent.Folder.Export.Visible = false
 	script.Parent.Parent.Parent.Panel:TweenPosition(UDim2.new(0,0,1,0))
